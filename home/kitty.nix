e88e142bdc32817fafd1d86a110fba98033f94c0
Rex <302173809+reqxdev@users.noreply.github.com>
2026-08-16 21:13:02 +0100

    ...

--- home/kitty.nix
+++ home/kitty.nix
@@ -1,3 +1,5 @@
+{ ... }:
+
 {
   programs.kitty = {
     enable = true;
@@ -5,6 +7,19 @@
     font = {
       name = "JetBrainsMono Nerd Font";
       size = 11;
+    };
+
+    settings = {
+      background = "#0b1018";
+      foreground = "#cdd6f4";
+
+      background_opacity = "0.76";
+      dynamic_background_opacity = "yes";
+
+      window_padding_width = 14;
+      hide_window_decorations = "yes";
+
+      confirm_os_window_close = 0;
     };
   };
 }
